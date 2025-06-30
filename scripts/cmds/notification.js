34c32b3c583f141eca256ac04ea86a21ecb89513
ᏦᎽᎾᎿᎯᏦᎯ <stromjersey@gmail.com>
2025-06-30 09:57:44 +0200

    Update notification.js

--- scripts/cmds/notification.js
+++ scripts/cmds/notification.js
@@ -1,5 +1,106 @@
 const { getStreamsFromAttachment } = global.utils;
+const { getStreamsFromAttachment } = global.utils;
 
+module.exports = {
+    config: {
+        name: "notification",
+        aliases: ["notify", "noti"],
+        version: "1.8",
+        author: "Dan Jersey",
+        countDown: 5,
+        role: 2,
+        description: {
+            vi: "Gửi thông báo từ admin đến all box",
+            en: "Send notification from admin to all box"
+        },
+        category: "owner",
+        guide: {
+            en: "{pn} <message>"
+        },
+        envConfig: {
+            delayPerGroup: 250
+        }
+    },
+
+    langs: {
+        en: {
+            missingMessage: "❌ Please enter the message you want to send to all groups",
+            notification: "📢 Notification from Kyotaka Admin (Do not reply to this message)",
+            sendingNotification: "⏳ Sending notification to %1 groups...",
+            sentNotification: "✅ Notification sent to %1 groups successfully",
+            errorSendingNotification: "❌ Error sending to %1 groups:\n%2"
+        }
+    },
+
+    onStart: async function ({ message, api, event, args, commandName, envCommands, threadsData, getLang }) {
+        const { delayPerGroup } = envCommands[commandName];
+        if (!args[0])
+            return message.reply(getLang("missingMessage"));
+
+        const userMessage = args.join(" ");
+
+        const formSend = {
+            body: `
+╭━━━━━━━━━━━━━━━━━━━━━━
+┃ ⚠️ ᴋʏᴏᴛᴀᴋᴀ - ɴᴏᴛɪꜰɪᴄᴀᴛɪᴏɴ
+┃━━━━━━━━━━━━━━━━━━━━━━
+┃ ${getLang("notification")}
+┃──────────────────────
+┃ ${userMessage}
+╰━━━━━━━━━━━━━━━━━━━━━━
+            `.trim(),
+            attachment: await getStreamsFromAttachment(
+                [
+                    ...event.attachments,
+                    ...(event.messageReply?.attachments || [])
+                ].filter(item => ["photo", "png", "animated_image", "video", "audio"].includes(item.type))
+            )
+        };
+
+        const allThreadID = (await threadsData.getAll()).filter(t => t.isGroup && t.members.find(m => m.userID == api.getCurrentUserID())?.inGroup);
+        message.reply(getLang("sendingNotification", allThreadID.length));
+
+        let sendSuccess = 0;
+        const sendError = [];
+        const waitingSend = [];
+
+        for (const thread of allThreadID) {
+            const tid = thread.threadID;
+            try {
+                waitingSend.push({
+                    threadID: tid,
+                    pending: api.sendMessage(formSend, tid)
+                });
+                await new Promise(resolve => setTimeout(resolve, delayPerGroup));
+            } catch (e) {
+                sendError.push(tid);
+            }
+        }
+
+        for (const sended of waitingSend) {
+            try {
+                await sended.pending;
+                sendSuccess++;
+            } catch (e) {
+                const { errorDescription } = e;
+                if (!sendError.some(item => item.errorDescription == errorDescription))
+                    sendError.push({
+                        threadIDs: [sended.threadID],
+                        errorDescription
+                    });
+                else
+                    sendError.find(item => item.errorDescription == errorDescription).threadIDs.push(sended.threadID);
+            }
+        }
+
+        let msg = "";
+        if (sendSuccess > 0)
+            msg += getLang("sentNotification", sendSuccess) + "\n";
+        if (sendError.length > 0)
+            msg += getLang("errorSendingNotification", sendError.reduce((a, b) => a + b.threadIDs.length, 0), sendError.reduce((a, b) => a + `\n - ${b.errorDescription}\n  + ${b.threadIDs.join("\n  + ")}`, ""));
+        message.reply(msg);
+    }
+};
 module.exports = {
         config: {
                 name: "notification",
